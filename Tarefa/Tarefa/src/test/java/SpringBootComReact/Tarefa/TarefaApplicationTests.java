8acee33219f9aac3914f7dc6472f6e343071bd56
Maria Clara de Araújo Monteiro <113060036+Kuria-chan18@users.noreply.github.com>
2024-04-17 18:59:31 -0300

    Add files via upload

---
+++ Tarefa/Tarefa/src/test/java/SpringBootComReact/Tarefa/TarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootComReact.Tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
